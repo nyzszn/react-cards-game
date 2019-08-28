db73b1ced6299da7e0d10dbf5929bf3e0c5d6a0a
Ian Nnyanzi <innyanzi@outbox.co.ug>
2019-08-28 23:18:54 +0300

    Pick from deck, Scenario player updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import _ from "lodash";
+import { async } from "q";
 
 const cards = require("./data/cards.json");
 
@@ -22,7 +23,10 @@ class App extends React.Component {
       nextPlayer: "",
       gameOver: true,
       winner: "",
-      requiredType:""
+      requiredType:"",
+      cardsToPick:1,
+      canPickFromDeck:false,
+      mustPickFromDeck:false
     };
   }
 
@@ -87,129 +91,208 @@ class App extends React.Component {
       gameOver: false
     });
   };
+  _handlePlayed = async (player, card, currentPlayedCards, nplayer) => {
+    switch( card.number ){
+      case 20:
+      // ask next player to pick 2 cards of play a an equivalent card`
+        this.setState({
+          played: currentPlayedCards,
+          cardsToPick:2,
+          canPickFromDeck:true,
+          mustPickFromDeck:true,
+          [player.name === "Player One"
+          ? "playerOne"
+          : "playerTwo"]: nplayer,
+          nextPlayer: player.name === "Player One" ? "Player Two" : "Player One"
+      });
+      break;
 
-  _playNext = async (player, card) => {
-    console.log("player", player);
-    //check to see if right player should play
-    if (player.name === this.state.nextPlayer) {
-      let currentPlayedCards = this.state.played;
-      const lastPlayedCard =
-        this.state.played.length > 0
-          ? this.state.played[this.state.played.length - 1]
-          : [];
-      if (this.state.played.length > 0) {
-        console.log("card", card);
-        console.log("lastplayed card", lastPlayedCard);
-        if (
-          card.text == lastPlayedCard.text ||
-          card.type == lastPlayedCard.type
-        ) {
-          console.log("Good to go!");
+      case 8:
+      // next player is themselves
+      this.setState({
+        played: currentPlayedCards,
+        cardsToPick:1,
+        canPickFromDeck:true,
+        mustPickFromDeck:false,
+        [player.name === "Player One"
+        ? "playerOne"
+        : "playerTwo"]: nplayer,
+        nextPlayer: player.name === "Player One" ? "Player One" : "Player Two"
+    });
+    break;
 
-          currentPlayedCards.push(card);
-          let nplayer =
-            player.name === "Player One"
-              ? this.state.playerOne
-              : this.state.playerTwo;
-          nplayer.cards = _.difference(player.cards, [card]);
-          this.setState({
-            played: currentPlayedCards,
-            [player.name === "Player One"
-              ? this.state.playerOne
-              : this.state.playerTwo]: nplayer,
-            nextPlayer:
-              player.name === "Player One" ? "Player Two" : "Player One"
-          });
-        } else {
-          console.log("Not the right card to play");
-        }
-      } else if (this.state.played.length == 0) {
+    case 11:
+    // next player is themselves
+    this.setState({
+      played: currentPlayedCards,
+      cardsToPick:1,
+      canPickFromDeck:true,
+      mustPickFromDeck:false,
+      [player.name === "Player One"
+      ? "playerOne"
+      : "playerTwo"]: nplayer,
+      nextPlayer: player.name === "Player One" ? "Player One" : "Player Two"
+  });
+  break;
+      case 15:
+      // ask next player to play type of choice next
+      this.setState({
+        played: currentPlayedCards,
+        cardsToPick:1,
+        canPickFromDeck:true,
+        mustPickFromDeck:false,
+        requiredType:this.state.requiredType,
+        [player.name === "Player One"
+        ? "playerOne"
+        : "playerTwo"]: nplayer,
+        nextPlayer: player.name === "Player One" ? "Player One" : "Player Two"
+    });
+    break;
+
+      // case card.number == 7 && card.type == this.state.cutter.type:
+      // // calculate if current player has to number of cards less that 25 in total if so then calculate each player who has the least to be the winner
+      // let totalCount = 0;
+      // this.state[player.name].cards.map((v, k)=>
+      //     totalCount +=v.number
+      // );
+      // if(totalCount <25){
+      //   this.setState({
+      //     played: currentPlayedCards,
+              // cardsToPick:1,
+              // canPickFromDeck:true,
+              // mustPickFromDeck:false,
+      //     requiredType:this.state.requiredType,
+      //      [player.name === "Player One"
+      //      ? "playerOne"
+      //      : "playerTwo"]: nplayer,
+      //     nextPlayer: player.name === "Player One" ? "Player One" : "Player Two"
+      // });
+      // // cut function to run after this
+      // }
+      // else{
+      //   console.log("Total is less than 25");
+      //   return false;
+      // }
+      
+      default:
+        this.setState({
+          played: currentPlayedCards,
+          cardsToPick:1,
+          canPickFromDeck:true,
+          mustPickFromDeck:false,
+          [player.name === "Player One"
+          ? "playerOne"
+          : "playerTwo"]: nplayer,
+          nextPlayer: player.name === "Player One" ? "Player Two" : "Player One"
+      });
+    }
+  }
+  _afterMath = async(player, card)=>{
+    let currentPlayedCards = this.state.played;
+    const lastPlayedCard =
+      this.state.played.length > 0
+        ? this.state.played[this.state.played.length - 1]
+        : [];
+    if (this.state.played.length > 0) {
+      console.log("card", card);
+      console.log("lastplayed card", lastPlayedCard);
+      if (
+        card.text == lastPlayedCard.text ||
+        card.type == lastPlayedCard.type
+      ) {
+        console.log("Good to go!");
+
         currentPlayedCards.push(card);
         let nplayer =
           player.name === "Player One"
             ? this.state.playerOne
             : this.state.playerTwo;
         nplayer.cards = _.difference(player.cards, [card]);
-        switch( card ){
-          case card.number == 2:
-          // ask next player to pick 2 cards of play a an equevalent card`
-            this.setState({
-              played: currentPlayedCards,
-              cardsToPick:2,
-              [player.name === "Player One"
-                ? this.state.playerOne
-                : this.state.playerTwo]: nplayer,
-              nextPlayer: player.name === "Player One" ? "Player Two" : "Player One"
-          });
-          case card.number == 8 || card.number == 11:
-          // next player is themselves
-          this.setState({
-            played: currentPlayedCards,
-            [player.name === "Player One"
-              ? this.state.playerOne
-              : this.state.playerTwo]: nplayer,
-            nextPlayer: player.name === "Player One" ? "Player One" : "Player Two"
-        });
-          case card.number == 15:
-          // ask next player to play type of choice next
-          this.setState({
-            played: currentPlayedCards,
-            requiredType:this.state.requiredType,
-            [player.name === "Player One"
-              ? this.state.playerOne
-              : this.state.playerTwo]: nplayer,
-            nextPlayer: player.name === "Player One" ? "Player One" : "Player Two"
-        });
-          case card.number == 7 && card.type == this.state.cutter.type:
-          // calculate if current player has to number of cards less that 25 in total if so then calculate each player who has the least to be the winner
-          let totalCount = 0;
-          this.state[player.name].cards.map((v, k)=>
-              totalCount +=v.number
-          );
-          if(totalCount <25){
-            this.setState({
-              played: currentPlayedCards,
-              requiredType:this.state.requiredType,
-              [player.name === "Player One"
-                ? this.state.playerOne
-                : this.state.playerTwo]: nplayer,
-              nextPlayer: player.name === "Player One" ? "Player One" : "Player Two"
-          });
-          // cut function to run after this
-          }
-          else{
-            console.log("Total is less than 25");
-            return false;
-          }
-          
-          default:
-            this.setState({
-              played: currentPlayedCards,
-              [player.name === "Player One"
-                ? this.state.playerOne
-                : this.state.playerTwo]: nplayer,
-              nextPlayer: player.name === "Player One" ? "Player Two" : "Player One"
-          });
-        }
+        await this._handlePlayed(player, card, currentPlayedCards, nplayer)
       } else {
+        console.log("Not the right card to play");
+      }
+    } else if (this.state.played.length == 0) {
+      currentPlayedCards.push(card);
+      let nplayer =
+        player.name === "Player One"
+          ? this.state.playerOne
+          : this.state.playerTwo;
+      nplayer.cards = _.difference(player.cards, [card]);
+       await this._handlePlayed(player, card, currentPlayedCards, nplayer)
+     } else {
         console.log("You ain't supposed to play 2");
         return false;
       }
-      //if true Migrate card from player cards to played cards at the end
-    } else {
+  }
+  _playNext = async (player, card) => {
+
+    //check to see if right player should play
+    if (player.name === this.state.nextPlayer && this.state.mustPickFromDeck == false) {
+      await this._afterMath(player, card);
+    } 
+    else if( player.name === this.state.nextPlayer && this.state.mustPickFromDeck == true && card.number === 20 ){
+      console.log("You are playing a 20 with a 20, nice one!");
+      await this._afterMath(player, card);
+    }
+
+    else if( this.state.mustPickFromDeck == true && card.number != 20  ){
+      console.log("You must pick from the deck");
+      return false;
+    }
+    else {
       console.log("You ain't supposed to play 1");
       return false;
     }
   };
 
-  _pickFromDeck = async (player, number =1) => {
+  _pickFromDeck = async () => {
+
+    if(this.state.canPickFromDeck){
+
+    
+    //must be the right player
+    let cardsToPick = this.state.cardsToPick;
     let cardsToAdd =[];
-    for (var i = 0; i < number; i++) {
-      cardsToAdd.push(this.state.deck[i]);
+    let canPickFromDeck = false;
+    let whatsLeftOftheDeck = this.state.deck;
+    let nplayer =
+          this.state.nextPlayer === "Player One"
+            ? this.state.playerOne
+            : this.state.playerTwo;
+
+      //determine whether the player has eaten a dog or not
+      if(this.state.mustPickFromDeck == true){
+        for (var i = 0; i < cardsToPick; i++) {
+          cardsToAdd.push(this.state.deck[i]);
+          nplayer.cards.push(this.state.deck[i])
+        }
+      }else{
+        //should only pick one card max
+        cardsToAdd.push(this.state.deck[0]);
+        nplayer.cards.push(this.state.deck[0])
+      }
+      
+      // remove cards from deck
+      whatsLeftOftheDeck = _.difference(whatsLeftOftheDeck, cardsToAdd);
+
+      // add cards to player cards
+      // upddate state not to require pick from cards{
+      this.setState({
+        // cannot pick again until played
+        canPickFromDeck:canPickFromDeck,
+        deck:whatsLeftOftheDeck,
+        [this.state.nextPlayer=== "Player One"
+            ? "playerOne"
+            : "playerTwo"]: nplayer,
+      });
+
+      console.log("Done picking from Deck")
     }
-    // remove cards from deck
-    // add cards to player cards
-    // upddate state not to require pick from cards
+    else{
+      console.log("Can't not pick from Deck")
+      return false;
+    }
 
   };
   _regroupDeckFromPlayed = async () => {
@@ -282,7 +365,7 @@ class App extends React.Component {
         {playerTwoCards}
         <hr />
         <h3>Deck</h3>
-        <span className="card">{this.state.deck.length}</span>
+        <span onClick={() => this._pickFromDeck()} className="card">{this.state.deck.length}</span>
         <hr />
         <h3>Cutter</h3>
         <div className="card">
